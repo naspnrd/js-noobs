dc9cee6c1ce77cbf1306384e87c07783c3e9a68d
Neeraj Chaudhary <neerajchaudhary@Neerajs-MacBook-Air.local>
2025-02-19 23:24:26 +0530

    class 9 code

---
+++ GoogleSheetClone_9_10_11/script.js
@@ -0,0 +1,42 @@
+const headRow = document.querySelector(".head-row");
+const headCol = document.querySelector(".serialNo");
+const body = document.querySelector(".body");
+const rows = 500,
+  columns = 27;
+
+(() => {
+  for (let i = 0; i < columns; i++) {
+    const headCell = document.createElement("div");
+    if (i === 0) {
+      // headRow.appendChild(headCell);
+      continue;
+    }
+
+    headCell.innerText = String.fromCharCode(i + 64); // A(65), B(66), C(67), D(68)
+    headCell.classList.add("col-head");
+    headRow.appendChild(headCell);
+  }
+})();
+
+(() => {
+  for (let i = 0; i < rows; i++) {
+    const headColCell = document.createElement("div");
+    headColCell.innerText = i + 1;
+    headColCell.classList.add("sno-cell");
+    headCol.appendChild(headColCell);
+  }
+})();
+
+(() => {
+  for (let row = 1; row <= rows; row++) {
+    const rowCells = document.createElement("div");
+    rowCells.classList.add("row");
+    for (let col = 1; col < columns; col++) {
+      const colCell = document.createElement("cite");
+      colCell.contentEditable = true;
+      colCell.classList.add("cell");
+      rowCells.appendChild(colCell);
+    }
+    body.appendChild(rowCells);
+  }
+})();
